383cb2ca08e8312a88049fffdba30987ffd38c26
marrinan <ryan.marrinan@gmail.com>
2013-03-20 21:38:23 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlocBlog::Application.config.secret_token = 'd7aceb2520788a036adeb05975d500e6f6cdfe29dc0db894827aace75f8354412ab3daf69ad79ac360f78f977665a9f8cd684577dbb5fddebb5b1cd25e40b460'
