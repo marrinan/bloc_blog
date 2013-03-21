3b173571596fa0ea4f9fb7d28ce0bdb8a45b3c9e
marrinan <ryan.marrinan@gmail.com>
2013-03-20 22:05:52 -0700

    improved_model_associations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ActiveRecord::Base
+  has_many :posts
+  has_many :comments
+  has_many :comments, through: :posts
+
   attr_accessible :bio, :email, :name, :password, :twitter, :website
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :author
+  has_many :comments
   attr_accessible :author_id, :body, :published_date, :title
 end
