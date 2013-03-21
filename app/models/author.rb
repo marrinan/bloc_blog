9c724a82704474255e95a54cdce4c1718c42400d
marrinan <ryan.marrinan@gmail.com>
2013-03-21 08:30:33 -0700

    comments_posts added to Author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ActiveRecord::Base
   has_many :posts
   has_many :comments
-  has_many :comments, through: :posts
+  has_many :comments_posts, class_name: 'Comment', through: :posts
 
   attr_accessible :bio, :email, :name, :password, :twitter, :website
 end
